33b01c587980f64fa50eaa06aacfe8bb0457c9d7
Marcel Gregoriadis <m.gregoriadis@gmail.com>
2025-05-12 19:46:23 +0200

    refactor

---
+++ scripts/get_ctrs.py
@@ -0,0 +1,61 @@
+import random
+import torch
+import numpy as np
+from joblib import Parallel, delayed
+from tqdm import tqdm
+from swarmsearch.datasets.aol import load_dataset
+from swarmsearch.utils.data import compile_clickthrough_records_as_arrays
+import pickle
+import lmdb
+import warnings
+import argparse
+
+warnings.filterwarnings(
+    "ignore",
+    message="A worker stopped while some jobs were given to the executor. This can be caused by a too short worker timeout or by a memory leak.",
+    category=UserWarning,
+    module="joblib.externals.loky.process_executor"
+)
+
+# Set random seeds for reproducibility
+random.seed(42)
+np.random.seed(42)
+torch.manual_seed(42)
+torch.cuda.manual_seed(42)
+if hasattr(torch, 'mps'):
+    torch.mps.manual_seed(42)
+torch.backends.cudnn.deterministic = True
+torch.backends.cudnn.benchmark = False
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Process AOL dataset with start and end indices')
+    parser.add_argument('--job-id', type=int, help='Slurm job ID')
+    parser.add_argument('--job-count', type=int, help='Total number of jobs')
+    args = parser.parse_args()
+    
+    df = load_dataset()
+    unique_users = sorted(df['user_id'].unique())
+    
+    # job filtering
+    user_ids = [u for i, u in enumerate(unique_users) if args.job_id is None or i % args.job_count == args.job_id]
+    user_groups = df[df['user_id'].isin(user_ids)].groupby('user_id')
+    del df, unique_users
+
+    ctr_arrays = Parallel(n_jobs=-1, batch_size=64)(
+        delayed(compile_clickthrough_records_as_arrays)(user_df) 
+        for _, user_df in tqdm(
+            user_groups,
+            total=len(user_groups),
+            desc=f"Processing users (job {args.job_id+1}/{args.job_count})" if args.job_id is not None else "Processing users"
+        )
+    )
+    
+    # Write to LMDB with user_id as key and list of ClickThroughRecords as value
+    print("Writing results to LMDB...")
+
+    with lmdb.open(f'data/ctrs_{args.job_id+1}.lmdb' if args.job_id is not None else 'data/ctrs.lmdb', map_size=2**46) as db:
+        with db.begin(write=True) as txn:
+            for user_id, ctrs in tqdm(zip(user_ids, ctr_arrays), total=len(user_ids), desc="Writing to LMDB"):
+                txn.put(str(user_id).encode(), pickle.dumps(ctrs))
+    
+    print("Saved to disk")
---
+++ scripts/get_docs.py
@@ -0,0 +1,52 @@
+import random
+import torch
+import numpy as np
+from tqdm import tqdm
+import pickle
+import lmdb
+import ir_datasets
+from swarmsearch.semantica.embed import embed_batch
+import argparse
+
+# Set random seeds for reproducibility
+random.seed(42)
+np.random.seed(42)
+torch.manual_seed(42)
+torch.cuda.manual_seed(42)
+if hasattr(torch, 'mps'):
+    torch.mps.manual_seed(42)
+torch.backends.cudnn.deterministic = True
+torch.backends.cudnn.benchmark = False
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Process AOL dataset with start and end indices')
+    parser.add_argument('--job-id', type=int, help='Slurm job ID')
+    parser.add_argument('--job-count', type=int, help='Total number of jobs')
+    args = parser.parse_args()
+
+    print("Loading dataset...")
+
+    dataset = ir_datasets.load("aol-ia")
+
+    start = args.job_id / args.job_count
+    end = (args.job_id + 1) / args.job_count
+
+    doc_id2content = {}
+    for doc in dataset.docs_iter()[start:end]:
+        doc_id2content[doc.doc_id] = f"{doc.url}\n{doc.title}\n{doc.text}"
+    
+    print("Embedding documents...")
+
+    # Call embed_batch. Inside embed_batch, each text is further split if needed.
+    # The "batch_size" argument inside embed_batch controls how many chunks go through the GPU at once. Adjust if needed.
+    # By experience: 64 consumes around 3.5G of memory, 256 consumes around 10G.
+    embeddings = embed_batch(doc_id2content.values(), batch_size=256)
+    
+    print("Saving to disk...")
+
+    with lmdb.open('data/doc_embeddings.lmdb', map_size=2**33, sync=False) as db: # 2**26=64M
+        with db.begin(write=True) as txn:
+            for doc_id, emb in tqdm(zip(doc_id2content.keys(), embeddings), total=len(doc_id2content)):
+                txn.put(str(doc_id).encode(), pickle.dumps(emb))
+
+    print("Done.")
